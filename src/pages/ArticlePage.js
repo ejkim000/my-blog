51f5936ebf38e84dedd29a186a64e821da11b5c1
ejkim000 <developer.ejk@gmail.com>
2023-08-22 16:47:04 -0400

    article page

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import { useParams } from "react-router-dom";
+import articles from "./article-content";
 
 function ArticlePage() {
+  const { articleId } = useParams();
+  // const articleId = params.articleId;
+  const article = articles.find((article) => article.name === articleId);
   return (
-    <div>ArticlePage</div>
-  )
+    <>
+      <h1>{article.title}</h1>
+      {article.content.map((p) => (
+        <p>{p}</p>
+      ))}
+    </>
+  );
 }
 
-export default ArticlePage
+export default ArticlePage;

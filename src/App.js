eb7f2c0ceb1b25940da4f24e730998af625464bb
ejkim000 <developer.ejk@gmail.com>
2023-08-22 16:36:37 -0400

    add routes and navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./NavBar";
+import AboutPage from "./pages/AboutPage";
+import HomePage from "./pages/HomePage";
+import ArticleListPage from "./pages/ArticleListPage";
+import ArticlePage from "./pages/ArticlePage";
 
 function App() {
   return (
-    <div className="App">
-      <h1> My Awesome Blog</h1>
-      <div id="page-body">Welcome to my blog!</div>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/articles" element={<ArticleListPage />} />
+            <Route path="/articles/:articleId" element={<ArticlePage />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 

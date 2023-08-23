fed6cd6a5b1be1b7f9fb02e74be6b41c4524c8df
ejkim000 <developer.ejk@gmail.com>
2023-08-23 10:54:23 -0400

    connect to server and db

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,9 +1,26 @@
+import { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
+import axios from 'axios';
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
 
 function ArticlePage() {
+  const [ articleInfo, setArticleInfo ] = useState({upvotes: 0, comments: []});
   const { articleId } = useParams();
+
+  useEffect(()=> {
+
+    const loadArticleInfo = async () => {
+      const response = await axios.get(`/api/articles/${articleId}`);
+      const newArticleInfo = response.data;
+      setArticleInfo(newArticleInfo);
+    }
+
+    loadArticleInfo();
+
+  }, []);
+
+  
   // const articleId = params.articleId;
   const article = articles.find((article) => article.name === articleId);
 
@@ -14,6 +31,7 @@ function ArticlePage() {
   return (
     <>
       <h1>{article.title}</h1>
+      <p>This article has {articleInfo.upvotes} upvote(s)</p>
       {article.content.map((paragraph, i) => (
         <p key={i}>{paragraph}</p>
       ))}
